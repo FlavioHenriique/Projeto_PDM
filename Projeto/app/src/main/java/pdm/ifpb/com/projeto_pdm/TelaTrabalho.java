cff1be7afd8ed32e0f26428fe44a11480d749308
FlavioHenriique <flaviohenrique638@gmail.com>
2018-05-24 17:22:09 -0300

    Adicionando fragmento da tela de cada trabalho

---
+++ Projeto/app/src/main/java/pdm/ifpb/com/projeto_pdm/TelaTrabalho.java
@@ -0,0 +1,26 @@
+package pdm.ifpb.com.projeto_pdm;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TelaTrabalho extends Fragment {
+
+
+    public TelaTrabalho() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_tela_trabalho, container, false);
+    }
+
+}
