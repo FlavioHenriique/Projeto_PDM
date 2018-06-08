c3de45b61bb833ac55a7f020bd57a0fea0a37c60
FlavioHenriique <flaviohenrique638@gmail.com>
2018-06-08 08:24:39 -0300

    Adicionando itens ao spinner da activity cadastro_usuario e uma intent na activity de login para abrir a activity de cadastro

--- Projeto/app/src/main/java/pdm/ifpb/com/projeto_pdm/Cadastro_usuario.java
+++ Projeto/app/src/main/java/pdm/ifpb/com/projeto_pdm/Cadastro_usuario.java
@@ -2,6 +2,8 @@ package pdm.ifpb.com.projeto_pdm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Cadastro_usuario extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class Cadastro_usuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_usuario);
+
+        Spinner spinner = findViewById(R.id.estado);
+        spinner.setPrompt("Selecione");
+
+        String[] arrayEstados = {"PB","PE","RJ","SP","SC"};
+        ArrayAdapter<String> adapter= new ArrayAdapter<>(this,
+                R.layout.support_simple_spinner_dropdown_item,arrayEstados);
+
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
--- Projeto/app/src/main/java/pdm/ifpb/com/projeto_pdm/Inicial.java
+++ Projeto/app/src/main/java/pdm/ifpb/com/projeto_pdm/Inicial.java
@@ -1,7 +1,10 @@
 package pdm.ifpb.com.projeto_pdm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Inicial extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class Inicial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicial);
+
+        TextView tv = findViewById(R.id.cadastrar);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Inicial.this,Cadastro_usuario.class);
+                startActivity(intent);
+            }
+        });
     }
 }
