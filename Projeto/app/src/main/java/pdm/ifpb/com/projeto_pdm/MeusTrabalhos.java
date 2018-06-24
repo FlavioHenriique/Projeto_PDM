d9288cd067aeb0ff5b9a2810b6f8d719dd211a50
FlavioHenriique <flaviohenrique638@gmail.com>
2018-06-24 14:22:11 -0300

    Listando trabalhos do usuário na tela de 'Meus Trabalhos'

--- Projeto/app/src/main/java/pdm/ifpb/com/projeto_pdm/MeusTrabalhos.java
+++ Projeto/app/src/main/java/pdm/ifpb/com/projeto_pdm/MeusTrabalhos.java
@@ -1,6 +1,8 @@
 package pdm.ifpb.com.projeto_pdm;
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,11 +12,29 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import pdm.ifpb.com.projeto_pdm.model.Trabalho;
+import pdm.ifpb.com.projeto_pdm.model.Usuario;
 
 
 public class MeusTrabalhos extends Fragment {
 
+
+    private Trabalho[] lista;
 
     public MeusTrabalhos() {
 
@@ -25,7 +45,11 @@ public class MeusTrabalhos extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view =  inflater.inflate(R.layout.fragment_meus_trabalhos, container, false);
+        View view =  inflater.inflate(R.layout.fragment_meus_trabalhos, container,
+                false);
+
+
+
         FloatingActionButton button = view.findViewById(R.id.addTrabalho);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +63,78 @@ public class MeusTrabalhos extends Fragment {
         return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        meusTrabalhos();
+    }
+
+    private void meusTrabalhos() {
+
+        OkHttpClient client = new OkHttpClient();
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+        String email = ((menu) getActivity()).getAtual().getEmail();
+        String url = "http://10.0.3.2:8080/pdm-api/pdm/trabalho/"+ email;
+
+        Request request = new Request.Builder().url(url).get().build();
+        Gson gson = new Gson();
+
+        try {
+            Response response = client.newCall(request).execute();
+            lista = gson.fromJson(response.body().string(), Trabalho[].class);
+
+            ListView lview = getView().findViewById(R.id.listaTrabalhos);
+
+            MyAdapter adapter = new MyAdapter(getContext());
+
+            lview.setAdapter(adapter);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+
+    private class MyAdapter extends BaseAdapter{
+
+        Context context;
+
+        public MyAdapter(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return lista.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return lista[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            View view = convertView;
+            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            view = inflater.inflate(R.layout.trabalhos, null);
+
+            TextView nome = view.findViewById(R.id.tNome);
+            TextView descricao = view.findViewById(R.id.tDescricao);
+
+            nome.setText(lista[position].getTitulo());
+            descricao.setText(lista[position].getDescricao());
+
+            return view;
+        }
+    }
+
+}
 
--- Projeto/app/src/main/java/pdm/ifpb/com/projeto_pdm/cadastro_trabalho.java
+++ Projeto/app/src/main/java/pdm/ifpb/com/projeto_pdm/cadastro_trabalho.java
@@ -2,12 +2,28 @@ package pdm.ifpb.com.projeto_pdm;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import pdm.ifpb.com.projeto_pdm.model.Trabalho;
 
 
 public class cadastro_trabalho extends Fragment {
@@ -25,6 +41,79 @@ public class cadastro_trabalho extends Fragment {
 
         return inflater.inflate(R.layout.fragment_cadastro_trabalho, container,
                 false);
-    }
 
+        }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        String[] categorias = {"Construção","Informática","Mecânica"};
+        String[] arrayEstados = {"PB","PE","RJ","SP","SC"};
+
+        final Spinner estados = getActivity().findViewById(R.id.estadoTrabalho);
+        final Spinner categoriasSpinner = getActivity().findViewById(R.id.categoriaTrabalho);
+
+        ArrayAdapter adapterEstado = new ArrayAdapter(this.getContext(),
+                R.layout.support_simple_spinner_dropdown_item,
+                arrayEstados);
+        adapterEstado.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        estados.setAdapter(adapterEstado);
+
+        ArrayAdapter adapterCategoria = new ArrayAdapter(this.getContext(),R.layout
+                .support_simple_spinner_dropdown_item, categorias);
+        adapterCategoria.setDropDownViewResource(R.layout
+                .support_simple_spinner_dropdown_item);
+        categoriasSpinner.setAdapter(adapterCategoria);
+
+        Button bt = getActivity().findViewById(R.id.btCadastroTrabalho);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText titulo = getActivity().findViewById(R.id.tituloTrabalho);
+
+                EditText cidade = getActivity().findViewById(R.id.cidadeTrabalho);
+                EditText valor = getActivity().findViewById(R.id.valorTrabalho);
+                EditText horario = getActivity().findViewById(R.id.horarioTrabalho);
+                EditText data = getActivity().findViewById(R.id.dataTrabalho);
+                EditText descricao = getActivity().findViewById(R.id.descricao);
+
+                Trabalho trabalho = new Trabalho();
+                trabalho.setCategoria(categoriasSpinner.getSelectedItem().toString());
+                trabalho.setCidade(cidade.getText().toString());
+                trabalho.setContratante(((menu) getActivity()).getAtual());
+                trabalho.setData(data.getText().toString());
+                trabalho.setDescricao(descricao.getText().toString());
+                trabalho.setHorario(horario.getText().toString());
+                trabalho.setTitulo(titulo.getText().toString());
+                trabalho.setEstado(estados.getSelectedItem().toString());
+                trabalho.setValor(Float.parseFloat(valor.getText().toString()));
+
+
+                Gson gson = new Gson();
+
+                OkHttpClient client = new OkHttpClient();
+                MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+                String url = "http://10.0.3.2:8080/pdm-api/pdm/trabalho";
+
+                RequestBody body = RequestBody.create(JSON, gson.toJson(trabalho));
+
+                Request request = new Request.Builder().url(url).post(body).build();
+
+                try {
+                    Response response = client.newCall(request).execute();
+
+                    if(response.code() == 201){
+                        Toast.makeText(getContext(), "Trabalho cadastrado",
+                                Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(getContext(), "Erro!",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 }
